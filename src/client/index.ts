68db9bcdba92f5d8de84189d4b895cc13578dc23
grpf <lastd@DESKTOP-M651MNU.localdomain>
2026-06-01 12:24:59 +0200

    Add C3L5

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,5 +1,33 @@
+import amqp from "amqplib";
+import { clientWelcome } from "../internal/gamelogic/gamelogic.js";
+import { declareAndBind, SimpleQueueType } from "../internal/pubsub/queue.js";
+import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+
 async function main() {
   console.log("Starting Peril client...");
+
+  const rabbitConnString = "amqp://guest:guest@localhost:5672/";
+  const conn = await amqp.connect(rabbitConnString);
+
+  if (conn) {
+    console.log("Connected to RabbitMQ");
+  }
+
+  ["SIGINT", "SIGTERM"].forEach((signal) =>
+    process.on(signal, async () => {
+      try {
+        await conn.close();
+        console.log("RabbitMQ connection closed.");
+      } catch (err) {
+        console.error("Error closing RabbitMQ connection:", err);
+      } finally {
+        process.exit(0);
+      }
+    }),
+  );
+
+  const userName = await clientWelcome();
+  await declareAndBind(conn, ExchangePerilDirect, `pause.${userName}`, PauseKey, SimpleQueueType.Transient);
 }
 
 main().catch((err) => {
