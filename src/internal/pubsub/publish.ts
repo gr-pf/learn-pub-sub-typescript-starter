f850c1d2fabe37a71d587b2232a6d0f5c1d7d344
grpf <lastd@DESKTOP-M651MNU.localdomain>
2026-06-07 18:56:35 +0200

    Add C6L1

--- src/internal/pubsub/publish.ts
+++ src/internal/pubsub/publish.ts
@@ -1,4 +1,5 @@
 import type { ConfirmChannel } from "amqplib";
+import { encode } from "@msgpack/msgpack";
 
 export async function publishJSON<T>(
     ch: ConfirmChannel,
@@ -25,3 +26,31 @@ export async function publishJSON<T>(
         );
     });
 };
+
+export async function publishMsgPack<T>(
+    ch: ConfirmChannel,
+    exchange: string,
+    routingKey: string,
+    value: T,
+): Promise<void> {
+
+    const encoded: Uint8Array = encode({ foo: "bar" });
+
+    const content: Buffer = Buffer.from(encoded.buffer, encoded.byteOffset, encoded.byteLength);
+
+    return new Promise((resolve, reject) => {
+        ch.publish(
+            exchange,
+            routingKey,
+            content,
+            { contentType: "application/x-msgpack" },
+            (err) => {
+                if (err !== null) {
+                    reject(new Error("Message was NACKed by the broker"));
+                } else {
+                    resolve();
+                }
+            },
+        );
+    });
+}
